977b7641d92210ec3478d2adae218a522988db63
halapichu <halapichu@gmail.com>
2025-04-19 09:07:38 -0700

    Create  app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, send_file
+from rembg import remove
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result_image = None
+    if request.method == 'POST':
+        file = request.files['image']
+        if file:
+            input_image = Image.open(file.stream)
+            output = remove(input_image)
+            byte_io = io.BytesIO()
+            output.save(byte_io, 'PNG')
+            byte_io.seek(0)
+            return send_file(byte_io, mimetype='image/png', as_attachment=True, download_name='no-bg.png')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
